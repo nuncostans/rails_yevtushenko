0faac1f7ca30bc562f0acfb94f33a8435584d849
nuncostans <yevova10@gmail.com>
2016-06-09 21:01:42 +0300

    Add cart to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603073241) do
+ActiveRecord::Schema.define(version: 20160609104627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160603073241) do
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
