5b27f454eae261e38bcbe4ceecb5748dbe10873c
nuncostans <yevova10@gmail.com>
2016-06-09 21:00:55 +0300

    Add association to product migration

--- db/migrate/20160603073241_create_products.rb
+++ db/migrate/20160603073241_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.belongs_to :cart, index: true
       t.string :name
       t.text :description
       t.float :price
