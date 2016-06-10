82b935784df560635b95348642e52f768563692e
nuncostans <yevova10@gmail.com>
2016-06-10 20:02:37 +0300

    Add edit and update to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :edit, :update]
+  
   resources :accounts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
