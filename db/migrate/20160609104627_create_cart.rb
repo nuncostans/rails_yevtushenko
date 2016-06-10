ac0d14892dc655aed14deccd566978fb031520d7
nuncostans <yevova10@gmail.com>
2016-06-10 15:33:19 +0300

    Changed migrations

--- db/migrate/20160609104627_create_cart.rb
+++ db/migrate/20160609104627_create_cart.rb
@@ -1,5 +1,13 @@
 class CreateCart < ActiveRecord::Migration
   def change
+    create_table :carts do |t|
+      t.string :product_name
+      t.float :product_price
+    end
+
+    create_table :products do |t|
+      t.belongs_to :carts, index: true
+    end
   end
   
 end
