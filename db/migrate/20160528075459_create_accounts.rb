1db0b159cf4b0104f0a5a9063b5d0d6c0fbb7700
nuncostans <yevova10@gmail.com>
2016-06-11 19:13:21 +0300

    Add friend references

--- db/migrate/20160528075459_create_accounts.rb
+++ db/migrate/20160528075459_create_accounts.rb
@@ -1,12 +1,10 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
+      t.references :friend, index: true
       t.string :name
       t.integer :age
       t.timestamps null: false
     end
-
-#    add_index :carts
-#    add_reference :accounts, :cart, index: true
   end
 end
