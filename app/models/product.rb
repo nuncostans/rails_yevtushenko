216c3862549cc9d78fb527d6a6ac7fc19d2f48a8
nuncostans <yevova10@gmail.com>
2016-06-11 19:11:48 +0300

    Add validations for name and price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
-#  belongs_to :cart
+  belongs_to :cart
+  
+  validates :name, presence: true, allow_blank: false
+  validates :price, presence: true, allow_blank: false, numericality: { greater_than: 0,
+                                                      message: "must be grater then 0"}
 end
