a5ecacbbf8ee088b489c0c0264f4de6ae612f6b7
nuncostans <yevova10@gmail.com>
2016-06-24 13:00:10 +0300

    Add simplecov config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start
+
 require 'shoulda/matchers'
 
 RSpec.configure do |config|
