8b6c97a3c27edde4cfb434e38d05de733166befc
nuncostans <yevova10@gmail.com>
2016-06-24 15:58:58 +0300

    Add helper product_list_partial_name

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,13 @@
 module ProductsHelper
   def alert_helper
   end
+
+  def product_list_partial_name
+    if Product.all.size == 0
+      return 'empty'
+    else
+      return 'list'
+    end
+  end
+  
 end
