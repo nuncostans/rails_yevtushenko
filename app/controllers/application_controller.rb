c7d8747e0fd811e0d62b3ed6f930e46740293cb0
nuncostans <yevova10@gmail.com>
2016-06-24 16:35:01 +0300

    Add helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :set_current_cart
+
   protect_from_forgery with: :exception
 
   private
 
   def set_current_cart
     session[:cart_id] ||= Cart.create.id
+  end
+  
+  def product_list_partial_name
+    if Product.all.size == 0
+      return 'empty'
+    else
+     return 'list'
+    end
   end
   
 end
