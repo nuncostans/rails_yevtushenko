4b6e05c89812afaf1e3bd869e44e30e9ffd90800
nuncostans <yevova10@gmail.com>
2016-06-30 11:00:22 +0300

    Delete unneed helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
 
-  def alert_helper
-    alert alert-info
-    returt 'alert alert-danger' if params[:error].present?
-    alert alert-succes
-  end
-
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,5 @@
 module ProductsHelper
 
-
-  def alert_helper
-  end
-
   def product_list_partial_name
     @products.empty? ? 'empty' : 'list'
   end
