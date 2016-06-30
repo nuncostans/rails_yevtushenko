acd84df5833b7579add3f36bca787a39cb8ac3fa
nuncostans <yevova10@gmail.com>
2016-06-30 12:18:25 +0300

    Change resource for singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-  resources :carts, only: :show
+  resource :cart, only: :show
  
   post '/carts/add_product', to: 'carts#add', as: 'add_to_cart'
 
