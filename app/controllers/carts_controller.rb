0f45bad5f483d158c6372e1cb8f10fb435962b7e
nuncostans <yevova10@gmail.com>
2016-06-30 10:58:49 +0300

    add cart_find method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,19 @@
 class CartsController < ApplicationController
-
+  
+  before_action :cart_find
+  
   def show
-    @cart = Cart.find(session[:cart_id])
+    @cart = cart_find.products
   end
   
   def add
-    @cart = Cart.find(session[:cart_id]).products << Product.find(params[:product_id])
+    @cart = cart_find.products << Product.find(params[:product_id])
+  end
+
+  private
+
+  def cart_find
+    @cart = Cart.includes(:products).find(session[:cart_id])
   end
   
 end
