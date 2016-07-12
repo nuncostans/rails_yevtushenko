dc92a8def5a775957a2c99393834a7ce69e6e720
nuncostans <yevova10@gmail.com>
2016-07-12 17:55:56 +0300

    Repair email preview

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -21,7 +21,7 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new(account_params)
     if @account.save
-      AccountMailer.welcome(@account).deliver_later
+      AccountMailer.welcome_email(@account).deliver_later
       flash[:success] = "Account #{@account.name} created!!!"
       redirect_to accounts_path
     else
