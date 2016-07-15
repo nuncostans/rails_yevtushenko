6bddb27b47e2a6e6c99e7c83d2f0b19daf249c9e
nuncostans <yevova10@gmail.com>
2016-07-16 02:04:47 +0300

    Add paperclip and set up it

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,10 @@
 class Account < ActiveRecord::Base
-  has_one    :cart
+  has_one :cart
   has_many :friendships
   has_many :friends, through: :friendships,
                      dependent: :destroy
   belongs_to :user
 
-  
-  validates :name, presence: true, allow_blank: false, format: {with: /(?!^\d+\z)\A.+\z/,
-                                                               message: "can't consists of only digits"}, length: {in: 8..20}
-  validates :age, presence: true, allow_blank: false, inclusion: {in: 18..100, message: "only from 18 to 100"}
+  validates :name, presence: true, allow_blank: false, format: { with: /(?!^\d+\z)\A.+\z/, message: "can't consists of only digits" }, length: { in: 8..20 }
+  validates :age, presence: true, allow_blank: false, inclusion: { in: 18..100, message: 'only from 18 to 100' }
 end
