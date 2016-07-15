80c2c4c10fb2f65ef3e39ee36145a092bed98dc5
nuncostans <yevova10@gmail.com>
2016-07-16 02:04:47 +0300

    Add paperclip and set up it

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,14 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @products =  Product.all
   end
 
   def show
   end
-  
+
   def new
     @product = Product.new
   end
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      flash[:success] = "Produt created!!!"
+      flash[:success] = 'Produt created!!!'
       redirect_to products_path
     else
       render 'new'
@@ -27,14 +27,14 @@ class ProductsController < ApplicationController
   end
 
   def update
-      if @product.update(product_params)
-        flash[:info] = 'Product was successfully updated'
-        redirect_to @product
-      else
-        render 'edit'
-      end
+    if @product.update(product_params)
+      flash[:info] = 'Product was successfully updated'
+      redirect_to @product
+    else
+      render 'edit'
+    end
   end
-  
+
   def destroy
     @product.destroy
     flash[:danger] = "You have destroyed product #{@product.name}"
@@ -42,13 +42,12 @@ class ProductsController < ApplicationController
   end
 
   private
-  
+
   def set_product
     @product = Product.find(params[:id])
   end
-  
+
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :photo)
   end
- 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713205310) do
+ActiveRecord::Schema.define(version: 20160715224914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,17 @@ ActiveRecord::Schema.define(version: 20160713205310) do
     t.string   "name"
     t.text     "description"
     t.float    "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "cart_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "products", ["cart_id"], name: "index_products_on_cart_id", using: :btree
