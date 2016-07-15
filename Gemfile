5bf2433421474ada66a4a780268d47c22f8d0d76
nuncostans <yevova10@gmail.com>
2016-07-15 23:57:37 +0300

    updated gems

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem 'paperclip', '~> 5.0.0'
 gem 'postmark-rails'
+gem 'warden'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
 
-
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
@@ -28,7 +27,10 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include Warden::Test::Helpers
+  config.before :suite do
+          Warden.test_mode!
+            end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
