2330715fe084c0af30e1f95a63464a22bd50bd6d
nuncostans <yevova10@gmail.com>
2016-07-17 16:14:36 +0300

    Add new controllers

---
+++ app/controllers/users/user.rb
@@ -0,0 +1,15 @@
+class Users::User < ApplicationController
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+  end
+end
