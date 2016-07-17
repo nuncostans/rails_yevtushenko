6a5848ed267db9add1089752781d41939d1c146e
nuncostans <yevova10@gmail.com>
2016-07-17 20:21:39 +0300

    Edit images config

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,21 @@ class Product < ActiveRecord::Base
   belongs_to :cart
 
   validates :name, presence: true, allow_blank: false
-  validates :price, presence: true, allow_blank: false, numericality: { greater_than: 0,
-                                                                        message: 'must be grater then 0' }
-  validates :description, presence: false, allow_blank: true
-  has_attached_file :photo, styles: { medium:'300x300>', thumb: '100x100>' },
+  validates :price,
+            presence: true,
+            allow_blank: false,
+            numericality: { greater_than: 0,
+                            message: 'must be grater then 0' }
+  validates :description,
+            presence: false,
+            allow_blank: true
+
+  has_attached_file :photo,
+                    styles: { medium: '300x300>', thumb: '100x100>' },
                     default_url: '/images/:style/missing.png'
-  #validates_attachment_content_type :photo, content_type: %r{/\Aimage\/.*\Z/}
+  # validates_attachment_content_type :photo, content_type: %r{/\Aimage\/.*\Z/}
   validates_attachment :photo,
-  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+                       content_type: { content_type: ['image/jpeg',
+                                                      'image/gif',
+                                                      'image/png'] }
 end
