09ff7222a0796b5e9e482406815b267033d183d8
nuncostans <yevova10@gmail.com>
2016-07-17 21:08:29 +0300

    Add admin role

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,7 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!
+  before_action :user_admin?
+  before_action :authorize_admin, except: :new
   before_action :set_account, only: [:show, :edit, :update, :destroy]
 
   def index
