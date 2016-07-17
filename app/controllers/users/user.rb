fce4eefb1e7bee7d7328fa09bca5ac2d1e67ba31
nuncostans <yevova10@gmail.com>
2016-07-17 16:28:31 +0300

    Egit omniauth conf

--- app/controllers/users/user.rb
+++ app/controllers/users/user.rb
@@ -1,15 +1,15 @@
-class Users::User < ApplicationController
+class Users::OmniauthCallbacksController < ApplicationController::OmniauthCallbacksController
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data["name"],
-    #        email: data["email"],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
+     unless user
+         user = User.create(name: data["name"],
+            email: data["email"],
+            password: Devise.friendly_token[0,20]
+         )
+     end
     user
   end
 end
