dc8f22bcdffc41be050d208a9007652e4710220d
nuncostans <yevova10@gmail.com>
2016-07-19 01:35:05 +0300

    Remove cloudary

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,8 +13,8 @@ class Product < ActiveRecord::Base
 
   has_attached_file :photo,
                     styles: { medium: '300x300>', thumb: '100x100>' },
-                    default_url: ':style/missing.png',
-                    storage: :cloudinary
+                    default_url: ':style/missing.png'
+#                    storage: :cloudinary
 
   # validates_attachment_content_type :photo, content_type: %r{/\Aimage\/.*\Z/}
   validates_attachment :photo,
