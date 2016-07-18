340250a7443a19211964fd2e4ac73b50adf24394
nuncostans <yevova10@gmail.com>
2016-07-18 16:29:10 +0300

    Edit config for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["599608173418-dtvnv5adj1g235t4n95pfpfm9gcmeb8r.apps.googleusercontent.com"], ENV["aAKQTMSscaX0nLFfOsvr2WK9"]
+ # provider :google_oauth2, ENV["599608173418-dtvnv5adj1g235t4n95pfpfm9gcmeb8r.apps.googleusercontent.com"], ENV["aAKQTMSscaX0nLFfOsvr2WK9"]
 end
