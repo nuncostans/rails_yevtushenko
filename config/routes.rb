026ef0e79827b4003f32b0c1e0ec66be51135465
nuncostans <yevova10@gmail.com>
2016-07-18 14:14:30 +0300

    Update routes callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users,
-             controllers: { omniauth_callbacks: "users/auth/google_oauth2/callback" }
+             controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resources :products
 
   resource :cart, only: :show do
