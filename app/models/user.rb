be2aba4733d490683ba9b3f31c740abf96da6c42
nuncostans <yevova10@gmail.com>
2016-07-18 15:26:36 +0300

    Edit omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
          :rememberable,
          :trackable,
          :validatable,
-         :omniauthable,
-         omniauth_providers: [:google_oauth2]
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   has_one :account
 
@@ -16,11 +15,11 @@ class User < ActiveRecord::Base
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    unless user
-      user = User.create(name: data['name'],
-                         email: data['email'],
-                         password: Devise.friendly_token[0, 20])
-    end
+    #unless user
+    #  user = User.create(name: data['name'],
+    #                     email: data['email'],
+    #                     password: Devise.friendly_token[0, 20])
+    #end
     user
   end
 end
