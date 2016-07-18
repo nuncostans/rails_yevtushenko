d884e58af9c1f43f9374433f851bc9fc03c05a09
nuncostans <yevova10@gmail.com>
2016-07-19 00:55:52 +0300

    User email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
-  
-  def welcome_user_email(user)
-    @user = account
-    @url = 'https://rails_yevtushenko.herokuapp.com'
-    mail(to: @user.name, subject: "Alloha, on this site")
-  end
 
+  def welcome_user_email(user)
+    @user = user
+    @url = 'https://rails_yevtushenko.herokuapp.com'
+    mail(to: @user.name, subject: "Alloha, on this site #{@url}")
+  end
 end
