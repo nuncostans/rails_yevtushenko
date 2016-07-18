b329ddd4099d94454e816b002ed354e895e9ce35
nuncostans <yevova10@gmail.com>
2016-07-18 14:05:31 +0300

    edit omniauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users,
+             controllers: { omniauth_callbacks: "/users/auth/google_oauth2/callback" }
   resources :products
 
   resource :cart, only: :show do
